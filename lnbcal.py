c5002f4f8f3a3b0505c1fb67b6f03f7d6661b273
SagarKuthe <121888763+SagarKuthe@users.noreply.github.com>
2023-01-25 22:57:22 -0800

    Delete lnbcal.py

--- lnbcal.py
+++
@@ -1,9 +0,0 @@
-num1=int(input("Enter your first Number"))
-num2=int(input("Enter your Second Number"))
-a=num1*num2
-print("Your Product is :-",a)
-if(a>500):
-    b=num1+num2
-    print(b)
-else:
-    print("Hello LNB Code is running Fine!!")
